64a33fd0832f74db439abca2806901bbd6e679c6
abinejohn <chrisabine8@gmail.com>
2023-03-03 03:13:56 -0800

    Python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Square module definition
+This module defines a simple `Square` class
+"""
+
+
+class Square:
+
+    """A simple ``Square`` class
+    Attribute:
+        size (`int`): The size of the ``Square``.
+    """
+
+    def __init__(self, size):
+        """Construct a ``Square`` Object.
+        Args:
+            size (`int`): The size of the ``Square``.
+        """
+        self._Square__size = size
